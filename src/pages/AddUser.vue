e84de1667e5dedfc88824f1458173a5d4dbda50f
BOKA <boka@192.168.8.111>
2020-08-05 18:12:45 +0800

    liusjadd

--- src/pages/AddUser.vue
+++ src/pages/AddUser.vue
@@ -14,13 +14,25 @@
         <div class="from-item">
           <div class="item-title">身份<span>*</span></div>
           <div class="item-cell">
-            <select class="bg-white" v-model="identity">
+            <!-- <select class="bg-white" v-model="identity">
               <option value="0">请选择身份</option>
               <option value="1">管理员</option>
               <option value="2">客户</option>
               <option value="3">供应商</option>
               <option value="4">业务员</option>
-            </select>
+            </select> -->
+            <el-dropdown @command="handleCommand" trigger="click">
+              <div class="el-dropdown-link">
+                {{groupname}}
+                <!-- <i class="el-icon-arrow-down el-icon--right"></i> -->
+              </div>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item :command="{name:'管理员',value:'1'}">管理员</el-dropdown-item>
+                <el-dropdown-item :command="{name:'客户',value:'2'}">客户</el-dropdown-item>
+                <el-dropdown-item :command="{name:'供应商',value:'3'}">供应商</el-dropdown-item>
+                <el-dropdown-item :command="{name:'业务员',value:'4'}">业务员</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
           </div>
         </div>
       </div>
@@ -42,14 +54,15 @@ export default {
     return {
       linkman: '',
       telephone: '',
-      identity: '0',
+      groupid: '0',
+      groupname: '请选择身份',
       issubmit: false
     }
   },
   methods: {
     submitEvent () {
       if (!this.issubmit) {
-        if (this.linkman === '' || this.telephone === '' || this.identity === '0') {
+        if (this.linkman === '' || this.telephone === '' || this.groupid === '0') {
           this.$vux.toast.text('必填项不能为空', 'middle')
         } else if (!(/^1[3456789]\d{9}$/).test(this.telephone)) {
           this.$vux.toast.text('请输入正确的手机号', 'middle')
@@ -58,7 +71,7 @@ export default {
           this.$http.post(`${ENV.BokaApi}/api/user/addMember`, {
             linkman: this.linkman,
             mobile: this.telephone,
-            groupid: this.identity
+            groupid: this.groupid
           }).then(res => {
             let data = res.data
             this.$vux.toast.text(data.error, 'middle')
@@ -70,6 +83,10 @@ export default {
     },
     toStatement () {
       this.$router.push({path: '/statement'})
+    },
+    handleCommand(command) {
+      this.groupid = command.value;
+      this.groupname = command.name
     }
   },
   activated () {
@@ -99,10 +116,13 @@ export default {
         color: #999;
         font-size: 13px;
       }
-      input,select,.popup-picker,.x-address{
+      input,select,.popup-picker,.x-address,.el-dropdown ,.el-dropdown-link{
         height: 100%;
         width: 100%;
         font-size: 15px;
+      }
+      .el-dropdown-link{
+        line-height: 50px;
       }
     }
   }
@@ -137,5 +157,9 @@ export default {
   .vux-popup-picker-select .vux-cell-value{color:initial !important;}
   .vux-cell-placeholder{color: #999 !important}
   .vux-popup-picker-select span{font-size: 15px !important}
+}
+.el-dropdown-menu{
+  width: 94%;
+  margin: 12px 20px;
 }
 </style>
