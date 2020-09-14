dec6086603ac8a95caa1d8e30812134a62731814
guoyp1985 <605034716@qq.com>
2020-09-14 17:13:11 +0800

    gypadd

--- src/pages/MakeDetails.vue
+++ src/pages/MakeDetails.vue
@@ -1151,7 +1151,7 @@ export default {
     },
     handleBtn () {
       this.controlBtn = []
-      if (this.viewData.status < 100 && (this.isManager || this.isSale)) {
+      if (this.viewData.status < 100 && this.isManager) {
         this.controlBtn.push({id: 1, title: '分配业务员', type: 'warning'})
       }
       if (this.viewData.status > 0 && (this.isCustomer)) {
