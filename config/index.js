6e738ab2e32321c5480d7d71267bae6ef5b75a47
guoyp1985 <605034716@qq.com>
2021-01-27 15:57:12 +0800

    gypadd

--- config/index.js
+++ config/index.js
@@ -55,10 +55,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../companybsa/index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../companybsa'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
